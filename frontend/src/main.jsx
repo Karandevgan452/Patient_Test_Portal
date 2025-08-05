522ea7e5bdf604a286ad15894f99b05c6dd80db0
name <karankarandevgan@gmail.com>
2025-08-05 14:52:21 +0530

    intital commit

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,12 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { BrowserRouter as Router} from 'react-router-dom'
+import { BrowserRouter as Route} from 'react-router-dom'
 
-createRoot(document.getElementById("root")).render(
+createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Router>
-      <App />
-    </Router>
-  </StrictMode>
-);
+    <App />
+  </StrictMode>,
+)
